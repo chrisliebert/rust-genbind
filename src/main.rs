1445998edb91446c8200c95a82faabed7a0d884f
chrisliebert <liebertx@gmail.com>
2017-08-09 13:34:38 -0700

    improved argument handling, and display program usage in 2 arguments are not supplied

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,34 @@
 extern crate genbind;
 
 pub fn main() {
-	use std::env;
-	// Default arguments
-	let mut input_file = String::from("src/tests/test.rs");
-	let mut output_file = String::from("myheader.h");
-	
-	if env::args().len() == 1 {
-		println!("Using default arguments, consider specifying input and output files");
-	}
-	
-	// Check for command-line arguments
-	let mut i = 0;
-	for argument in env::args() {
+    use std::env;
+    // Default arguments
+    let mut input_file = String::from("src/tests/test.rs");
+    let mut output_file = String::from("myheader.h");
+    
+    let args: Vec<String> = env::args().collect();
+    
+    // argument 0 is the executable name
+    if args.len() != 3 {
+        println!("Usage: {} <input_source>.rs <output_header>.h", args[0]);
+    }
+    
+    // Check for command-line arguments
+    let mut i = 0;
+    for argument in args {
         if i == 1 {
             input_file = argument;
         } else if i == 2 {
-        	output_file = argument;
+            output_file = argument;
         }
         i += 1;
     }
-	
-	genbind::write_c_header(
-		&std::path::Path::new(&input_file),
-		&std::path::Path::new(&output_file)
-	);
+    println!("Reading source from {}", &input_file);
+    
+    genbind::write_c_header(
+        &std::path::Path::new(&input_file),
+        &std::path::Path::new(&output_file)
+    );
+    
+    println!("Wrote {}", &output_file);
 }
